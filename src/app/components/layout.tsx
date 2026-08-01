a0ecb0129cf4d28b36ee3b75585ebebe351b6d81
MohammedArmaaan <193485809+MohammedArmaaan@users.noreply.github.com>
2026-08-01 18:13:21 +0530

    Propered The Site-10

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./header";
 import { Footer } from "./footer";
 import { MobileNavigation } from "./mobile-navigation";
 import { Seo } from "./seo";
 
 export function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.key]);
+
   return (
     <div className="relative min-h-screen flex flex-col overflow-x-hidden" style={{ fontFamily: "'Poppins', sans-serif" }}>
       <Seo />
